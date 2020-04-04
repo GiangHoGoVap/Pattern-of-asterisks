96fc9bda5416f9de0a665beb2b8f78f6e3be2343
GiangHoGoVap <58210406+GiangHoGoVap@users.noreply.github.com>
2020-04-04 21:37:01 +0700

    Add files via upload

---
+++ PatternOfAsterisks2.cpp
@@ -0,0 +1,33 @@
+/*
+    *
+   **
+  ***
+ ****
+*****
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	int N,i,j,k;
+	
+	do{
+		cout << "Input the number of rows: ";
+		cin >> N;
+		if (!cin.good()){
+			cin.clear();
+			cin.ignore(1024,'\n');
+		}
+	}while(N<=0);
+	
+	for (i=N;i>=1;i--){
+		for (j=1;j<i;j++){
+			cout << " ";
+		}
+		for(k=N;k>=i;k--){
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
